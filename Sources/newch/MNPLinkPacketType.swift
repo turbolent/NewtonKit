a111eb06be062fb489a0b11ca7dd54d7b68d4518
Bastian Müller <bastian@turbolent.com>
2018-04-11 20:41:00 -0700

    move packet type to separate file

---
+++ Sources/newch/MNPLinkPacketType.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+// MNP / V.42 error correction T-REC-V.42-199303, Annex A
+
+// TABLE A.1/V.42: Header-field types
+
+internal enum MNPPacketType: UInt8 {
+    case LR = 1   // Link request
+    case LD = 2   // Link disconnect
+    // NOTE: no 3
+    case LT = 4   // Link transfer
+    case LA = 5   // Link acknowledgement
+}
--- Sources/newch/MNPPacket.swift
+++ Sources/newch/MNPPacket.swift
@@ -5,14 +5,3 @@ public protocol MNPPacket {
     init(data: Data) throws
     func encode() -> Data
 }
-
-
-// TABLE A.1/V.42: Header-field types
-
-internal enum MNPPacketType: UInt8 {
-    case LR = 1   // Link request
-    case LD = 2   // Link disconnect
-    // NOTE: no 3
-    case LT = 4   // Link transfer
-    case LA = 5   // Link acknowledgement
-}
