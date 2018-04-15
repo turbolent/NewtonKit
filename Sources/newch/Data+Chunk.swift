4f1af0f6a1518130562861b4e4f2e00ed61f2660
Bastian Müller <bastian@turbolent.com>
2018-04-14 21:51:30 -0700

    add data debugging utilities like hex dump

---
+++ Sources/newch/Data+Chunk.swift
@@ -0,0 +1,13 @@
+
+import Foundation
+
+extension Data {
+    public func chunk(n: Int) -> [Data] {
+        return stride(from: 0, to: count, by: n)
+            .map { startOffset in
+                let startIndex = self.startIndex.advanced(by: startOffset)
+                let endIndex = Swift.min(startIndex.advanced(by: n), self.endIndex)
+                return subdata(in: startIndex..<endIndex)
+            }
+    }
+}
---
+++ Sources/newch/Data+Debug.swift
@@ -0,0 +1,40 @@
+
+import Foundation
+
+extension Data {
+    
+    public var hexLine: String {
+        return map { String(format: "%02x", $0) }
+            .joined(separator: " ")
+    }
+
+    public var hexDump: String {
+        let subChunkWidth = 8
+        func pad(_ subChunk: String) -> String {
+            return subChunk.padding(toLength: subChunkWidth * 3 - 1,
+                                    withPad: " ",
+                                    startingAt: 0)
+        }
+        return chunk(n: subChunkWidth * 2)
+            .enumerated()
+            .map { offset, chunkData in
+                let offsetFormatted = String(format: "%08x", offset)
+                let subChunks = chunkData.chunk(n: subChunkWidth)
+                let subChunk1Formatted =
+                    pad(subChunks[0].hexLine)
+                let subChunk2Formatted =
+                    pad(subChunks.count > 1 ? subChunks[1].hexLine : "")
+                let humanReadable =
+                    String(format: "|%@|", chunkData.map {
+                        return 32..<126 ~= $0 ? String(UnicodeScalar($0)) : "."
+                        }.joined())
+                return [
+                    offsetFormatted,
+                    subChunk1Formatted,
+                    subChunk2Formatted,
+                    humanReadable
+                    ].joined(separator: "  ")
+            }
+            .joined(separator: "\n")
+    }
+}
