6caf1e597192b7acecdefaf2aac5c40ed2adcb8f
Bastian Müller <bastian@turbolent.com>
2018-04-21 13:25:22 -0700

    no need to decode keys in parts

--- Sources/newch/NewtonInfoPacket.swift
+++ Sources/newch/NewtonInfoPacket.swift
@@ -6,29 +6,20 @@ public struct NewtonInfoPacket: DecodableDockPacket {
 
     public static let command: DockCommand = .newtonInfo
 
+    private static let protocolVersionSize = MemoryLayout<UInt32>.size
+    private static let encryptedKeySize = 8
+    private static let requiredSize = protocolVersionSize + encryptedKeySize
+
     public enum DecodingError: Error {
         case invalidSize
         case invalidProtocolVersion
-        case invalidEncryptedKey1
-        case invalidEncryptedKey2
     }
 
     public let protocolVersion: UInt32
-    public let encryptedKey1: UInt32
-    public let encryptedKey2: UInt32
-
-    public init(protocolVersion: UInt32,
-                encryptedKey1: UInt32,
-                encryptedKey2: UInt32) {
-
-        self.protocolVersion = protocolVersion
-        self.encryptedKey1 = encryptedKey1
-        self.encryptedKey2 = encryptedKey2
-    }
+    public let encryptedKey: Data
 
     public init(data: Data) throws {
-        let fieldSize = MemoryLayout<UInt32>.size
-        guard data.count == 3 * fieldSize else {
+        guard data.count == NewtonInfoPacket.requiredSize else {
             throw DecodingError.invalidSize
         }
 
@@ -37,23 +28,12 @@ public struct NewtonInfoPacket: DecodableDockPacket {
         else {
             throw DecodingError.invalidProtocolVersion
         }
+        self.protocolVersion = protocolVersion
 
-        guard
-            let encryptedKey1 =
-                UInt32(bigEndianData: data.advanced(by: fieldSize))
-        else {
-            throw DecodingError.invalidEncryptedKey1
-        }
-
-        guard
-            let encryptedKey2 =
-                UInt32(bigEndianData: data.advanced(by: 2 * fieldSize))
-        else {
-            throw DecodingError.invalidEncryptedKey2
-        }
-
-        self.init(protocolVersion: protocolVersion,
-                  encryptedKey1: encryptedKey1,
-                  encryptedKey2: encryptedKey2)
+        let encryptedKeyStartIndex =
+            data.startIndex.advanced(by: NewtonInfoPacket.protocolVersionSize)
+        let encryptedKeyEndIndex =
+            encryptedKeyStartIndex.advanced(by: NewtonInfoPacket.encryptedKeySize)
+        encryptedKey = data.subdata(in: encryptedKeyStartIndex..<encryptedKeyEndIndex)
     }
 }
--- Sources/newch/PasswordPacket.swift
+++ Sources/newch/PasswordPacket.swift
@@ -6,47 +6,33 @@ public struct PasswordPacket: CodableDockPacket {
 
     public static let command: DockCommand = .password
 
+    private static let encryptedKeySize = 8
+
     public enum DecodingError: Error {
         case invalidSize
-        case invalidEncryptedKey1
-        case invalidEncryptedKey2
     }
 
-    public let encryptedKey1: UInt32
-    public let encryptedKey2: UInt32
+    public let encryptedKey: Data
 
-    public init(encryptedKey1: UInt32,
-                encryptedKey2: UInt32) {
-
-        self.encryptedKey1 = encryptedKey1
-        self.encryptedKey2 = encryptedKey2
+    public init(encryptedKey: Data) {
+        self.encryptedKey = encryptedKey
     }
 
     public init(data: Data) throws {
-        let fieldSize = MemoryLayout<UInt32>.size
-        guard data.count == 2 * fieldSize else {
+        guard data.count == PasswordPacket.encryptedKeySize else {
             throw DecodingError.invalidSize
         }
 
-        guard let encryptedKey1 = UInt32(bigEndianData: data) else {
-            throw DecodingError.invalidEncryptedKey1
-        }
+        let encryptedKeyStartIndex = data.startIndex
+        let encryptedKeyEndIndex =
+            encryptedKeyStartIndex.advanced(by: PasswordPacket.encryptedKeySize)
+        let encryptedKey =
+            data.subdata(in: encryptedKeyStartIndex..<encryptedKeyEndIndex)
 
-        guard
-            let encryptedKey2 =
-                UInt32(bigEndianData: data.advanced(by: fieldSize))
-        else {
-            throw DecodingError.invalidEncryptedKey2
-        }
-
-        self.init(encryptedKey1: encryptedKey1,
-                  encryptedKey2: encryptedKey2)
+        self.init(encryptedKey: encryptedKey)
     }
 
     public func encode() -> Data? {
-        var data = Data()
-        data.append(encryptedKey1.bigEndianData)
-        data.append(encryptedKey2.bigEndianData)
-        return data
+        return encryptedKey
     }
 }
