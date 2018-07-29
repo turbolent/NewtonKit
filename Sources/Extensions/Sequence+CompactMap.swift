b3b427a4eb8fab72d8745dc64b141fa6275a598a
Bastian Müller <bastian@turbolent.com>
2018-07-29 00:04:13 +0000

    make extension public

--- Sources/Extensions/Sequence+CompactMap.swift
+++ Sources/Extensions/Sequence+CompactMap.swift
@@ -1,8 +1,8 @@
 
 #if swift(>=4.1)
 #else
-extension Sequence {
-    func compactMap<ElementOfResult>(_ transform: (Self.Element) throws -> ElementOfResult?)
+public extension Sequence {
+    public func compactMap<ElementOfResult>(_ transform: (Self.Element) throws -> ElementOfResult?)
         rethrows -> [ElementOfResult]
     {
         return try flatMap(transform)
